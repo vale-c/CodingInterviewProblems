6fea26a378eba97df382c501df8fd0663ca2542b
Vale <vale@MacBook-Pro-001.homenet.telecomitalia.it>
2019-03-25 21:48:08 +0100

    added Tree problem Solution

---
+++ JavaScript Solutions/invertTree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+
+  if (root === null) return null;
+
+  let tmp = root.left;
+  root.left = invertTree(root.right);
+  root.right = invertTree(tmp);
+
+  return root;
+
+};
