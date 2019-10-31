acebdd6870314907b817ad31860a4e00390e792e
vale-c <valent95@gmail.com>
2019-10-31 12:39:05 +0100

    added 344 and 70

---
+++ LeetCode/Java/70.climbing-stairs.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=70 lang=java
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+    public int climbStairs(int n) {
+        int result = 0;
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[n-1]+dp[n-2];
+            result = dp[i];
+        }
+        return result;
+    }
+}
+// @lc code=end
+
