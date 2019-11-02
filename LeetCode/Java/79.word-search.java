64965cd822b54e57f97149bde352068cc2b0024f
Vale <vale@MacBook-Pro.homenet.telecomitalia.it>
2019-11-02 10:32:00 +0100

    added woord search 79

--- LeetCode/Java/79.word-search.java
+++ LeetCode/Java/79.word-search.java
@@ -2,7 +2,6 @@
  * @lc app=leetcode id=79 lang=java
  *
  * [79] Word Search
-<<<<<<< HEAD
  *
  * https://leetcode.com/problems/word-search/description/
  *
@@ -35,68 +34,41 @@
  * Given word = "ABCB", return false.
  * 
  * 
-=======
->>>>>>> acebdd6870314907b817ad31860a4e00390e792e
  */
 
 // @lc code=start
 class Solution {
-<<<<<<< HEAD
-    public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word)) {
-=======
-
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word)){
->>>>>>> acebdd6870314907b817ad31860a4e00390e792e
                     return true;
                 }
             }
         }
         return false;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> acebdd6870314907b817ad31860a4e00390e792e
     // DFS approach
     public boolean dfs(char board[][], int i, int j, int count, String word) {
         if (count == word.length()) {
             return true;
         }
 
-<<<<<<< HEAD
-        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(count)) {
-=======
         if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(count)){
->>>>>>> acebdd6870314907b817ad31860a4e00390e792e
             return false;
         }
 
         char temp = board[i][j];
         board[i][j] = ' ';
 
-<<<<<<< HEAD
-        boolean found = dfs(board, i + 1, j, count + 1, word) || dfs(board, i - 1, j, count + 1, word)
-                || dfs(board, i, j + 1, count + 1, word) || dfs(board, i, j - 1, count + 1, word);
-=======
         boolean found = dfs(board, i+1, j, count+1, word)
         || dfs(board, i-1, j, count+1, word)
         || dfs(board, i, j+1, count+1, word)
         || dfs(board, i, j-1, count+1, word);
->>>>>>> acebdd6870314907b817ad31860a4e00390e792e
 
         board[i][j] = temp;
         return found;
     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> acebdd6870314907b817ad31860a4e00390e792e
 // @lc code=end
 
